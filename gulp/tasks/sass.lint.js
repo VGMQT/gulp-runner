a610b36ea3cfb023ca2b489df0b510a7211b391b
Vlad Gerasimovich <vg.email.2@gmail.com>
2019-08-27 12:56:28 +0300

    update sass.lint and .stylelintrc

--- gulp/tasks/sass.lint.js
+++ gulp/tasks/sass.lint.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('sass:lint', function(cb) {
-    $.exec(`yarn stylelint --fix ${$.config.dev}/styles/**/*.scss`, function (error, stdout) {
+    $.exec(`yarn stylelint --fix ${$.config.dev}/styles/*.scss ${$.config.dev}/styles/**/*.scss`, function (error, stdout) {
       console.log(stdout);
       cb(error);
     });
