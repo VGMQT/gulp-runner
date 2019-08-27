4240ed6d94b83f11bbc0e7f2459887046ea01a6b
Vlad Gerasimovich <vg.email.2@gmail.com>
2019-08-27 12:26:37 +0300

    update watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,7 +4,7 @@ module.exports = function() {
   $.gulp.task('watch', function() {
     // $.gulp.watch($.config.dev + '/js/**/*.js', $.gulp.series('js:app'));
     $.gulp.watch($.config.dev + '/js/**/*.js', $.gulp.series('js:app-minify'));
-    $.gulp.watch([$.config.dev + '/styles/**/*.scss', $.config.dev + '/styles/**/*.sass'], $.gulp.series('sass'));
+    $.gulp.watch([$.config.dev + '/styles/**/*.scss', $.config.dev + '/styles/**/*.sass'], $.gulp.series('sass:lint', 'sass'));
     // $.gulp.watch($.config.dev + '/*.html', $.gulp.series('html'));
     $.gulp.watch($.config.dev + '/pug/**/*.pug', $.gulp.series('pug'));
     $.gulp.watch($.config.dev + '/fonts/**/*.*', $.gulp.series('fonts'));
