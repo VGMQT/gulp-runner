b5cffeb3dc0999cb5d8ead23bc044efe1250a5d8
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2019-09-17 10:50:55 +0300

    update browserslist and fix eslint task

--- gulp/tasks/js.lint.js
+++ gulp/tasks/js.lint.js
@@ -1,10 +1,10 @@
 module.exports = () => {
   $.gulp.task('js:lint', cb => {
-    $.exec('pretty-quick', (error, stdout) => {
+    $.exec('yarn pretty-quick', (error, stdout) => {
       console.log(stdout);
       cb(error);
     });
-    $.exec(`eslint '${$.config.dev}/js/**/*.js' --fix`, (error, stdout) => {
+    $.exec(`yarn eslint '${$.config.dev}/js/**/*.js' --fix`, (error, stdout) => {
       console.log(stdout);
       cb(error);
     });
